66e65d785b7880a8eb79d078df5d7e5e6af25aea
Hishe Hintsa Fisseha <Hintsa@Hishes-MacBook-Pro.local>
2019-02-21 07:24:31 +0900

    [hintsa] created files and edited pp_09]

--- CH8/pp_hintsa/ch08project09.c
+++ CH8/pp_hintsa/ch08project09.c
@@ -1,170 +1,164 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#define MAX 10
-#define MAX_VALUE 'Z'
-#define DIRECTION	4
-#define FALSE	0
-#define TRUE	1
-#define UP		0
-#define DOWN	1
-#define LEFT	2
-#define	RIGHT	3
-#define ERROR	-1
-#define EMPTY	'*'
-#if 1
-#define LOG		FALSE
-#else
-#define LOG		TRUE
-#endif
-#define PRINT(args...) \
-		do { if (LOG) \
-			printf(args); } while(0)
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
+#define SIZE 10
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
 
-int isAble(char arr[][MAX], int x, int y, int direction){
-	int ret = FALSE;
-	if(arr[x][y] + 1 > MAX_VALUE)
-		return ret;
-	switch(direction){
+char randArray[SIZE][SIZE];
+bool isLetter[SIZE][SIZE] = {false};
+int x = 0, y = 0;
+void InitArray (){
+
+	int i, j;
+	for(i = 0; i < SIZE; i++){
+		for(j = 0; j < SIZE; j++){
+			randArray[i][j] = '.';
+			//printf("%c",randArray[i][j]);
+		}
+		//printf("\n");
+	}
+
+	return;
+}
+
+int checkAllSideLetter (int x, int y){
+	bool T_all, B_all, R_all, L_all, C_all;
+	if(x == 0 && isLetter[x][y+1] == true && isLetter[x][y-1] == true && isLetter[x+1][y] == true){
+		T_all = true;
+		//printf("Exited t\n");
+	}
+
+	else
+		T_all = false;
+
+	if(x == 9 && isLetter[x][y+1] == true && isLetter[x][y-1] == true && isLetter[x-1][y] == true){
+		B_all = true;
+		//printf("Exited b\n");
+	}
+	else
+		B_all = false;
+	if(y == 0 && isLetter[x][y+1] == true && isLetter[x+1][y] == true && isLetter[x-1][y] == true){
+		L_all = true;
+		//printf("Exited l\n");
+	}
+	else
+		L_all = false;
+	if(y == 9 && isLetter[x][y-1] == true && isLetter[x+1][y] == true && isLetter[x-1][y] == true){
+		R_all = true;
+		//printf("Exited r\n");
+	}
+	else
+		R_all = false;
+	if(isLetter[x][y+1] == true && isLetter[x][y-1] == true && isLetter[x+1][y] == true && isLetter[x-1][y] == true){
+		C_all = true;
+		//printf("Exited c\n");
+	}
+	else
+		C_all = false;
+	if(T_all || B_all || R_all || L_all || C_all)
+		return 1;
+	else
+		return 0;
+
+}
+
+int checkGo (int direction){
+
+	switch (direction) {
 		case UP:
-			if(y == 0) {
-				ret = FALSE;
-			}
-			else if(arr[x][y-1] != EMPTY) {
-				ret = FALSE;
-			}
-			else {
-				ret = TRUE;
-			}
-			break;
+			//printf("0");
+			if (x == 0 || isLetter[x-1][y] != 0)
+				return 0;
+			else
+				return 1;
 		case DOWN:
-			if(y == MAX - 1) {
-				ret = FALSE;
-			}
-			else if(arr[x][y+1] != EMPTY) {
-				ret = FALSE;
-			}
-			else {
-				ret = TRUE;
-			}
-			break;
+			//printf("1");
+			if (x == 9 || isLetter[x+1][y] != 0)
+				return 0;
+			else
+				return 1;		
 		case LEFT:
-			if(x == 0) {
-				ret = FALSE;
-			}
-			else if(arr[x-1][y] != EMPTY) {
-				ret = FALSE;
-			}
-			else {
-				ret = TRUE;
-			}
-			break;
+			//printf("2");
+			if (y == 0 || isLetter[x][y-1] != 0)
+				return 0;
+			else
+				return 1;
 		case RIGHT:
-			if(x == MAX - 1) {
-				ret = FALSE;
-			}
-			else if(arr[x+1][y] != EMPTY) {
-				ret = FALSE;
-			}
-			else {
-				ret = TRUE;
-			}
-			break;
+			//printf("3");
+			if (y == 9 || isLetter[x][y+1] != 0)
+				return 0;
+			else
+				return 1;
 		default:
-			PRINT("%s:%d, current point : (%d,%d), error", __FUNCTION__, __LINE__, x, y);
-			break;
+			printf("INPUT ERROR");
 	}
-	return ret;
-}
-int getRandomDirection(){
-	int random = rand() % DIRECTION;
-	return random;
-}
-int goRandomDirection(char arr[][MAX], int x, int y, char value){
-	int dir[DIRECTION] = {0,};
-	int randomDirection;
-	int i;
-	char cValue = value;
-	int bufX = x, bufY = y;
-	int isNeedMore = 0;
-	while(1){
-		if(!isNeedMore){
-			arr[bufX][bufY] = cValue;
-			PRINT("arr[%d][%d] : %c\n", bufX, bufY, arr[bufX][bufY]);
-			cValue++;
-		}
-		randomDirection = getRandomDirection();
-		if(isAble(arr, bufX, bufY, randomDirection)){
-			switch(randomDirection){
-				case UP:
-					bufY--;
-					break;
-				case DOWN:
-					bufY++;
-					break;
-				case LEFT:
-					bufX--;
-					break;
-				case RIGHT:
-					bufX++;
-					break;
-				default:
-					exit(1);
-					break;
-			}
-			isNeedMore = 0;
-			for(i = 0; i < DIRECTION; i++) dir[i] = 0;//clean
-		}
-		else{
-			isNeedMore = 1;
-			dir[randomDirection] = 1;
-			int count = 0;
-			for(i = 0; i < DIRECTION; i++){
-				if(dir[i] == 1){ 
-					count++;
-				}
-			}
-			if(count == DIRECTION){
-				PRINT("end\n");
-				for(i = 0; i < DIRECTION; i++){
-					PRINT("count : %d, dir[%d] : %d\n", count, i, dir[i]);
-				}
-				break;
-			}
-			if(cValue > MAX_VALUE){
-				PRINT("end\n");
-				break;
-			}
-		}
-	}
-}
-
-void initArray(char arr[][MAX], int maxX, int maxY){
-	int i, j;
-	for(i = 0; i < maxX; i++){
-		for(j = 0; j < maxY; j++){
-			arr[i][j] = EMPTY;
-		}
-	}
-}
-	
-void printArray(char arr[][MAX], int maxX, int maxY){
-	int i, j;
-	for(i = 0; i < maxX; i++){
-		for(j = 0; j < maxY; j++){
-			printf("%c", arr[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-int main(){
-	int arr[MAX][MAX] = {0,};
-	srand((unsigned)time(NULL)); // To Initialize before get random number
-	initArray(arr, MAX, MAX);
-	goRandomDirection(arr, 0, 0, 'A');
-	printArray(arr, MAX, MAX);
 	return 0;
 }
 
+void putLetter (){
+	int direction;
+	int random;
+	srand((unsigned) time(NULL));
+	randArray[x][y] = 'A';
+	isLetter[x][y] = true;
+	while (randArray[x][y] < 'A'+25){
+		direction = rand ()%4;
+		//printf("%c\n",randArray[x][y]);
+		//printf("%d\n",direction);
+		if(!checkGo(direction))
+			continue;
+		switch (direction){
+			case DOWN:
+				randArray[x+1][y] = randArray[x][y]+1;
+				isLetter[x+1][y] = true;
+				x++;
+				break;
+			case UP:
+				randArray[x-1][y] = randArray[x][y]+1;
+				isLetter[x-1][y] = true;
+				x--;
+				break;
+			case LEFT:
+				randArray[x][y-1] = randArray[x][y]+1;
+				isLetter[x][y-1] = true;
+				y--;
+				break;
+			case RIGHT:
+				randArray[x][y+1] = randArray[x][y]+1;
+				isLetter[x][y+1] = true;
+				y++;
+				break;
+			default:
+				printf("INPUT ERROR");
+		}
+
+		if(checkAllSideLetter(x,y))
+			break;
+
+	}
+	return;
+}
+
+void Output (){
+	int i, j;
+	for(i = 0; i < 10; i++){
+		for(j = 0; j < 10; j++){
+			printf("%c", randArray[i][j]);
+		}
+		printf("\n");
+	}
+	return;
+}
+
+
+int main (){
+	InitArray();
+	putLetter();
+	Output();
+	return 0;
+}
